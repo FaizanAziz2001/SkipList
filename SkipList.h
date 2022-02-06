5f328f96890a2b7b1da4000620340a7e10ac0ed1
FaizanAziz2001 <98259868+FaizanAziz2001@users.noreply.github.com>
2022-02-06 19:32:51 +0500

    Update SkipList.h

--- SkipList.h
+++ SkipList.h
@@ -7,7 +7,7 @@ class SkipList
 {
 	struct Node
 	{					
-		Node* up;											//a single node with all connections and a key
+		Node* up;				//a single node with all connections and a key
 		Node* down;
 		Node* left;
 		Node* right;
@@ -35,10 +35,10 @@ public:
 	{
 		srand(static_cast<unsigned int>(time(NULL)));				//seed for random
 		current_level = 0;
-		Node* starting_head = new Node(INT_MIN);					//to handle boundry cases
+		Node* starting_head = new Node(INT_MIN);			    	//to handle boundry cases
 		Node* starting_tail = new Node(INT_MAX);
 
-		starting_head->right = starting_tail;						//initial head and tail
+		starting_head->right = starting_tail;					//initial head and tail
 		starting_tail->left = starting_head;
 		head.push_back(starting_head);
 		tail.push_back(starting_tail);
@@ -46,14 +46,14 @@ public:
 
 	Node* CreateNode(int val, int level)
 	{
-		int index = level;											//creates a node at a given level,does not deal with up and down links
+		int index = level;							//creates a node at a given level,does not deal with up and down links
 		Node* insert = new Node(val);
 		Node* itr = head[index];
 
 		while (itr->right != nullptr && val > itr->val)				//iteration
 			itr = itr->right;
 
-		insert->right = itr;										//left and right connections
+		insert->right = itr;							//left and right connections
 		insert->left = itr->left;
 
 		itr->left->right = insert;
@@ -70,7 +70,7 @@ public:
 		{
 			if (val == itr->val)
 				return itr;
-			if (val > itr->val && val < itr->right->val)						//check the current node val and node->right val to determine the cases
+			if (val > itr->val && val < itr->right->val)			//check the current node val and node->right val to determine the cases
 			{
 				if (itr->down)
 				{
@@ -86,43 +86,43 @@ public:
 	void Insert(int val)
 	{
 		int level = 0;
-		Node* insert = CreateNode(val, 0);						//insert a node at bottom row
+		Node* insert = CreateNode(val, 0);					//insert a node at bottom row
 
 		while (RandomLevel() == 1)							
 		{
-			level++;															//increase level index if coin is 1
+			level++;							//increase level index if coin is 1
 			if (level > current_level)
 			{
-				current_level++;												//increase total level number
-				Node* new_head = new Node(INT_MIN);								//if upper level does not exist,create new tail and head
+				current_level++;					//increase total level number
+				Node* new_head = new Node(INT_MIN);			//if upper level does not exist,create new tail and head
 				Node* new_Tail = new Node(INT_MAX);
 
 				new_head->down = head[level - 1];
 				head[level - 1]->up = new_head;
 
 				new_Tail->down = tail[level - 1];
-				tail[level - 1] = new_Tail;										//make upper and downwards connection
+				tail[level - 1] = new_Tail;				//make upper and downwards connection
 
 				new_head->right = new_Tail;
 				new_Tail->left = new_head;
 
 				head.push_back(new_head);
-				tail.push_back(new_Tail);										//push the new head and tail
+				tail.push_back(new_Tail);				//push the new head and tail
 			}
 
-			insert->up = CreateNode(val, level);								//create a new node at upper level
+			insert->up = CreateNode(val, level);				//create a new node at upper level
 			insert->up->down = insert;
-			insert = insert->up;												//move to upper node as the loop can continue and a new node can be create on top of that layer
+			insert = insert->up;						//move to upper node as the loop can continue and a new node can be create on top of that layer
 		}
 	}
 
 	void Print()
 	{
-		Node* itr;																//start printing from top row
+		Node* itr;								//start printing from top row
 		for (int i = 0; i < head.size(); i++)
 		{
 			itr = head[current_level - i];
-			while (itr != nullptr)										//iteration
+			while (itr != nullptr)						//iteration
 			{
 				cout << itr->val << " ";
 				itr = itr->right;
@@ -133,17 +133,17 @@ public:
 
 	bool Delete(int val)
 	{
-		Node* del = Search(val);										//delete the first searched value
-		if (del == nullptr)												//if val does not exist,return
+		Node* del = Search(val);						//delete the first searched value
+		if (del == nullptr)							//if val does not exist,return
 			return false;
 
 		Node* L = del->left;
 		Node* R = del->right;
 
-		L->right = del->right;											//left right new connections
+		L->right = del->right;							//left right new connections
 		R->left = del->left;
 
-		if (del->down)												//up down new connections
+		if (del->down)								//up down new connections
 		{
 			del->down->up = nullptr;
 			del->down = nullptr;
@@ -155,12 +155,12 @@ public:
 			del->up = nullptr;
 		}
 
-		delete del;											//free memory
+		delete del;								//free memory
 		return true;
 	}
 
 	int RandomLevel()
 	{
-		return rand() % 2;										//return a random number between 1 and 0
+		return rand() % 2;							//return a random number between 1 and 0
 	}
 };
